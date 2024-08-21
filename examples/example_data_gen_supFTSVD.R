c610a4aa236c13c0f9a409f310ae743e373c15d1
ma521 <ma521@bstat-k02x50q5.lan>
2024-08-21 00:48:22 -0400

    full package

---
+++ examples/example_data_gen_supFTSVD.R
@@ -0,0 +1,57 @@
+# feature dimension 
+
+pdim<-c(500)
+
+# No of rank-1 components
+lr<-2
+r<-lr
+
+# Basis dimension
+lmd_val<-c(120,80) #lmd_min*(r:1)
+
+# weight for reminder
+sig<-0
+
+# noise variance in tensor
+Tau2<-1
+
+# Parameters for supervised component
+Eta2<-c(1,1.5) # error variance
+gam<-as.matrix(cbind(c(1.5,3),c(2,3.4))) # partial slopes
+
+
+
+# Grid of Time points
+nres<-51
+Time<-seq(0,1,length.out=nres)
+#Pht<-PhiF[[1]](Tg)
+set.seed(nres)
+PhiFunc<-lapply(1:r, function(x){
+  function(s){
+    a<-t(sapply(s,function(j){
+      c(1,sapply(2:10, function(i){sqrt(2)*cos((i-1)*(pi)*j)}))
+    }))%*%matrix(sapply(1:10,function(j){runif(1,-1/j,1/j)}))
+    a/norm(a,type="2")
+  }
+})
+PhiF<-sapply(1:r,function(r){sqrt(length(Time))*PhiFunc[[r]](Time)})
+
+set.seed(pdim)
+Bval<<-sapply(1:r, function(b){runif(pdim)})
+bval<<-Bval*outer(rep(1,pdim),1/apply(Bval,2,norm,type="2"))
+
+
+# Grid scenario
+
+Mi<-3:8
+
+n<-50
+m_i<-sample(Mi,n,replace = TRUE)
+set.seed(n)
+Vmat<<-cbind(round(runif(n),2),round(rbeta(n,1,1),2))
+
+
+data<-data_gen_supFTSVD(m_i=m_i,Xmatrix = Vmat,Beta = gam,Xi = bval,
+                  PsiF = PhiF,sing_val = lmd_val,SubE_Var = Eta2,
+                  Data_Var = Tau2,Tgrid = Time)
+
